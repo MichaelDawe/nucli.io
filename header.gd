f8a48d540d1e022acd988057b6f3e3eae3960759
Michael Dawe <75983563+MichaelDawe@users.noreply.github.com>
2024-12-20 19:04:58 +0000

    restructured and added search page and made header separate from content

---
+++ header.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+var search = false
+
+
+var searchpage = preload("res://search.tscn")
+
+
+func _on_theme_pressed() -> void:
+	get_parent()._on_theme_pressed()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("enter"):
+		if $VBoxContainer/HBoxContainer/SearchBarText.has_focus():
+			var query = $VBoxContainer/HBoxContainer/SearchBarText.text
+			var s = searchpage.instantiate()
+			s.query = query
+			get_parent().get_node("Content").free()
+			get_parent().add_child(s)
+			get_parent().move_child(s, 0)
+
+
+func _on_search_button_pressed() -> void:
+	var query = $VBoxContainer/HBoxContainer/SearchBarText.text
+	var s = searchpage.instantiate()
+	s.query = query
+	get_parent().get_node("Content").free()
+	get_parent().add_child(s)
+	get_parent().move_child(s, 0)
--- main.gd
+++ main.gd
@@ -2,7 +2,6 @@ extends Control
 
 var _theme = 0
 var themes = [preload("res://dark_theme.tres"), preload("res://light_theme.tres"), preload("res://user_theme.tres")]
-var styleboxes = [preload("res://dark_theme_search_stylebox.tres"), preload("res://light_theme_search_stylebox.tres"), preload("res://user_theme_search_stylebox.tres")]
 
 
 var page = 0 # 0 = Main, 1 = Game, etc...
@@ -39,6 +38,8 @@ func _ready() -> void:
 	# language menu
 	var popup = $Header/VBoxContainer/HBoxContainer/Language.get_popup()
 	popup.connect("id_pressed", language_menu)
+	# reset the scroll container
+	$Content/ScrollContainer.scroll_vertical = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -52,18 +53,18 @@ func _on_theme_pressed() -> void:
 		0:
 			theme = themes[1]
 			$Gradient/TextureRect.modulate = Color.WHITE
-			$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[1])
+			#$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[1])
 			_theme = 1
 		1:
 			theme = themes[2]
 			$Gradient/TextureRect.modulate = Color(1, 0.5, 0.69)
-			$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[2])
+			#$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[2])
 			$Content/TextureBackground.visible = true
 			_theme = 2
 		2:
 			theme = themes[0]
 			$Gradient/TextureRect.modulate = Color.BLACK
-			$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[0])
+			#$Header/VBoxContainer/HBoxContainer/SearchBar.add_theme_stylebox_override("normal", styleboxes[0])
 			$Content/TextureBackground.visible = false
 			_theme = 0
 
